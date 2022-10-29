39449a25846b0f69b0fbaeac9866e63e3a9248b2
Aziz Limonu <azizlimonu@gmail.com>
2022-10-29 11:30:08 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Routes } from 'react-router-dom';
+import { Footer, Navbar } from './components';
+import { Home, SignIn, SignUp, Account, CoinPage } from './pages';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/signin' element={<SignIn />} />
+        <Route path='/singup' element={<SignUp />} />
+        <Route path='/account' element={<Account />} />
+        <Route path='/coinpage' element={<CoinPage />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export {default as CoinItem} from './CoinItem';
+export {default as CoinSearch} from './CoinSearch';
+export {default as Footer} from './Footer';
+export {default as Navbar} from './Navbar';
+export {default as SavedCoin} from './SavedCoin';
+export {default as ThemeToggle} from './ThemeToggle';
+export {default as Trending} from './Trending';
